cbe2f36976f62e51401164c548d73a1d1a1a287e
DESKTOP-OHM88DD\YSK <yoonsg1007@gmail.com>
2022-11-13 20:37:36 +0900

    2022-11-13 splash 화면, 초기 로그인 화면

---
+++ OrderApp2/app/src/main/java/com/example/orderapp/SplashActivity.kt
@@ -0,0 +1,22 @@
+package com.example.orderapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+    internal var handler = Handler()
+
+    /** Called when the activity is first created.  */
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        handler.postDelayed({
+            val intent = Intent(baseContext, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
+    }
+}
